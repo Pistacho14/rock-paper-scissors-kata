b44b87d2658d00b9b4e535c2ec9c9c9088871e43
Pistacho14 <pablogonzaleztemes@gmail.com>
2025-02-06 20:07:41 +0100

    feat: :sparkles: Replace ifs with dictionary

--- main.py
+++ main.py
@@ -21,43 +21,26 @@ class MainGame:
         game_choices_str = ", ".join(game_choices)
         user_selection = int(input(f"\nPick a choice ({game_choices_str}): "))
         self.user_action = GameAction(user_selection)
-
+        print(self.user_action)
         return self.user_action
-    
 
     def assess_game(self, user_action, computer_action):
         if user_action == computer_action:
             print("It's a tie!")
             return GameResult.TIE
 
-        elif user_action == GameAction.ROCK:
-            if computer_action == GameAction.SCISSOR:
-                print("Rock smashes scissors. You won!")
-                return GameResult.VICTORY
-            else:
-                print("Paper covers rock. You lost!")
-                return GameResult.DEFEAT
+        elif computer_action == dic_actions.get(user_action):
+            print("You won!")
+            return GameResult.VICTORY
+        else:
+            print("You loose!")
+            return GameResult.DEFEAT
 
-        elif user_action == GameAction.PAPER:
-            if computer_action == GameAction.ROCK:
-                print("Paper covers rock. You won!")
-                return GameResult.VICTORY
-            else:
-                print("Scissors cuts paper. You lost!")
-                return GameResult.DEFEAT
-
-        elif user_action == GameAction.SCISSOR:
-            if computer_action == GameAction.PAPER:
-                print("Paper covers rock. You won!")
-                return GameResult.VICTORY
-            else:
-                print("Rock smashes scissors. You lost!")
-                return GameResult.DEFEAT
 
     def play_another_round(self):
         another_round = input("\nAnother round? (y/n): ")
         return another_round.lower() == 'y'
-    
+
 
     def main(self):
         while True:
